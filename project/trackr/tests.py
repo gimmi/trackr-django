2702d04ba14142f72e27d7416e57dc71e0ff2b88
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2013-10-05 19:20:05 +0200

    refined test

--- project/trackr/tests.py
+++ project/trackr/tests.py
@@ -8,19 +8,14 @@ class ItemTest(TestCase):
 	def test_create_and_retrieve_items(self):
 		self.assertEqual(Item.objects.count(), 0)
 
-		tag1 = Tag.objects.create(name='tag1')
-		# tag2 = Tag.objects.create(name='tag2')
-
 		item = Item()
 		item.title = 'item 1'
 		item.body = 'item 1 body'
 		item.save()
-		item.tags.add(tag1)
-		item.tags.create(name='tag2')
-		item.save()
 
 		self.assertEqual(Item.objects.count(), 1)
-		self.assertEqual(Tag.objects.count(), 2)
+		item = Item.objects.get(id=item.id)
+		self.assertIsInstance(item, Item)
 
 	def test_create_tag(self):
 		self.assertEqual(Tag.objects.count(), 0)
