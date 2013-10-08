76b19c27b0549bbbd316a29daf1b5fc6065793f2
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2013-10-08 23:29:21 +0200

    /posts/ show also tags

--- project/trackr/serializers.py
+++ project/trackr/serializers.py
@@ -3,12 +3,13 @@ from rest_framework import serializers
 
 from trackr.models import Item, Tag
 
-class ItemSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Item
-		fields = ('id', 'title', 'body', 'tags')
-
 class TagSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Tag
 		fields = ('id', 'name')
+
+class ItemSerializer(serializers.ModelSerializer):
+	tags = TagSerializer(many=True)
+	class Meta:
+		model = Item
+		fields = ('id', 'title', 'body', 'tags')
